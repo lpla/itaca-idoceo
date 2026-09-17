63f4e7fcbd90fbeff921bfacf3f5818274369ed2
Leopoldo Pla Sempere <lpla@dlsi.ua.es>
2026-09-17 18:08:42 +0200

    Test final 0.8.0 version on PR #2

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,6 +5,6 @@ from itaca_idoceo import core
 
 
 def test_package_and_legacy_manifest_versions_match():
-    assert itaca_idoceo.__version__ == "0.8.0a1"
+    assert itaca_idoceo.__version__ == "0.8.0"
     assert core.SCRIPT_VERSION == itaca_idoceo.__version__
     assert version("itaca-idoceo") == itaca_idoceo.__version__
